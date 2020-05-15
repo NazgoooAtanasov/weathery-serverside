ce2f08f6b317b3c206d7700ce0be4e31aeafcecd
NazgoooAtanasov <naskodragomirov@gmail.com>
2020-05-16 01:03:23 +0300

    Wrote documentation for the ApiController.

--- src/Weathery.ServerSide/Weathery.API/Controllers/ApiController.cs
+++ src/Weathery.ServerSide/Weathery.API/Controllers/ApiController.cs
@@ -4,12 +4,17 @@
     using System.Security.Claims;
     using Microsoft.AspNetCore.Mvc;
 
+    /// <summary>Base controller for the application.</summary>
     [Route("api/[controller]")]
     [ApiController]
     public abstract class ApiController : ControllerBase
     {
+        /// <summary>Gets the currently logged user's id.</summary>
+        /// <returns>The users id.</returns>
         protected string GetLoggedUserId() => this.HttpContext.User?.Claims?.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier).Value;
 
+        /// <summary>Checks wether there is logged user.</summary>
+        /// <returns>True of false.</returns>
         protected bool IsAuthenticated() => this.HttpContext.User.Identity.IsAuthenticated;
     }
 }
